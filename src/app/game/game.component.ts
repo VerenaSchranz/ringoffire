3d1cbdc06a3b7b51ab4591ef98a016cd08a7c2bf
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-08 06:00:53 +0200

    Update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,20 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, Game],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent {
 pickCardAnimation = false;
-game: Game;
+game: Game = new Game();
+
+
+constructor() {
+}
+
+  ngOnInit() {
+    this.newGame();
+  }
 
   newGame() {
     this.game = new Game();
+    console.log(this.game);
   }
 
   takeCard() {
