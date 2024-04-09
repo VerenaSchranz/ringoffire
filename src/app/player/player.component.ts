8f1027d505543e1e01c5289c1dde76e3996b6a95
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-09 11:40:43 +0200

    Update

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,13 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { GameComponent } from '../game/game.component';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, GameComponent],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss'
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit{
+  ngOnInit(): void {
+
+  }
   @Input() playerActive: boolean = false;
   @Input() player!: string;
 }
